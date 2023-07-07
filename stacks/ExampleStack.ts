9676804cb96c420fe2da6a2b7165253f0f608a2b
Joshua Goon <j.j.jappiecraft@gmail.com>
2023-07-07 01:20:37 -0500

    unused

--- stacks/ExampleStack.ts
+++ stacks/ExampleStack.ts
@@ -24,21 +24,8 @@ export function ExampleStack({ stack, app }: StackContext) {
 		},
 	});
 
-	const site = new StaticSite(stack, "Site", {
-		path: "packages/frontend",
-		buildCommand: "npm run build",
-		buildOutput: "dist",
-		environment: {
-			VITE_APP_AUTH0_DOMAIN: process.env.AUTH0_DOMAIN!,
-			VITE_APP_AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID!,
-			VITE_APP_API_URL: api.url,
-			VITE_APP_REGION: app.region,
-		},
-	});
-
 	// Show the API endpoint and other info in the output
 	stack.addOutputs({
 		ApiEndpoint: api.url,
-		SiteUrl: site.url,
 	});
 }
